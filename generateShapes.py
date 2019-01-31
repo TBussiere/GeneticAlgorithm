b306be41c929fa3c03e521d3c61bebc44f18355f
BUSSIERE THIBAULT p1601465 <thibault.bussiere@etu.univ-lyon1.fr>
2019-01-31 18:23:12 +0100

    creation des commantaire

--- generateShapes.py
+++ generateShapes.py
@@ -2,41 +2,58 @@ import pybullet as p
 import time
 import random
 
-
+# créé la fenetre de base
 p.connect(p.GUI)
+# créé le sol
 p.createCollisionShape(p.GEOM_PLANE)
 p.createMultiBody(0, 0)
-
-
+# met une seed au rng
 random.seed(time.time)
-
+# Defini le nombre d'objet pour la "RandomShape"
 nbobj = 5
 
+# defini un radius de base
 rad = 0.05
-ratation = [0, 0, 0, 1]
-pos1 = [10*rad, 10*rad, 10*rad]
-
-body = p.createCollisionShape(p.GEOM_SPHERE, radius=rad)
-colBoxId = p.createCollisionShape(
-    p.GEOM_BOX, halfExtents=[rad, rad, rad])
-
-# parts = []
+# defini une mass de base
 mass = 1
+# defini une rotation de base
+ratation = [0, 0, 0, 1]
+# defini une position de base
+pos1 = [10*rad, 10*rad, 10*rad]
+# créé la sphere qui sera le point centrale de la "Shape"
+body = p.createCollisionShape(p.GEOM_SPHERE, radius=rad)
 
+# ==========================================================
+# Creation des matrices pour link les autres shapes au body
+# ==========================================================
+# poid du nouveau l'objet
 linkMass = []
+# CollisionShape du nouveau objet
 linkColind = []
+# position relative du nouveau objet
 linkpos = []
+# oriantation du nouveau objet
 linkRotate = []
+# je sais pas trop a quoi sa sert mais si on le met pas sa plante
 idk1 = []
+# je sais pas trop a quoi sa sert mais si on le met pas sa plante
 idk2 = []
+# defini une texture -1 pour pas default
 vShapeInd = []
-# l'objet 0(carré) est link au 0 du main body (boule)
+# l'objet index i est link au chiffre n+1 mis dans la liste
+# Exemple [0,1] => Link 0 qui est le body avec 0+1 qui est le 1er objet generé
+#               Et Link le 1er objet genéré avec le second
 linkInd = []
+# acepte une emum de la lib exemple :
 # p.JOINT_PRISMATIC
 # p.JOINT_REVOLUTE
 joinType = []
+# defini les axes de liberté du join defini avant
 joinAxis = []
 
+# ==========================================================
+# Generation des nouvelles shapes
+# ==========================================================
 
 for i in range(0, nbobj):
 
@@ -71,6 +88,9 @@ for i in range(0, nbobj):
         [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)])
 
 
+# ==========================================================
+# Creation du Multi Body Aka Le truc qui devra bouger
+# ==========================================================
 shape = p.createMultiBody(body, mass, -1, pos1, ratation,
                           linkMasses=linkMass,
                           linkCollisionShapeIndices=linkColind,
@@ -85,30 +105,15 @@ shape = p.createMultiBody(body, mass, -1, pos1, ratation,
                           )
 
 
+# ==========================================================
+# Truc lié a la simulation
+# ==========================================================
+
+# met la gravité
 p.setGravity(0, 0, -7)
+# enleve la pause
 p.setRealTimeSimulation(1)
+# Le "run"
 while (1):
     keys = p.getKeyboardEvents()
     time.sleep(0.5)
-
-
-"""
-OLD
-
-nb_links = nbobj
-linkMass = [0.1]
-linkColind = [colBoxId]
-
-linkpos = [[1, 0, 0]]
-linkRotate = [[0, 0, 0, 1]] * nb_links
-idk1 = [[0, 0, 0]] * nb_links
-idk2 = [[0, 0, 0, 1]] * nb_links
-vShapeInd = [-1] * nb_links
-# l'objet 0(carré) est link au 0 du main body (boule)
-linkInd = [0]
-# p.JOINT_PRISMATIC
-# p.JOINT_REVOLUTE
-#
-joinType = [p.JOINT_PRISMATIC]
-joinAxis = [[1, 0, 0]]
-"""
